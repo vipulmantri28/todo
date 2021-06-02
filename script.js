308a31eb067da052da8f0e43ab77f4230da1c07c
Vipul Mantri <vipulmantri@Vipuls-MacBook-Pro.local>
2021-06-02 07:22:23 +0530

    array oriented approach applied

--- script.js
+++ script.js
@@ -1,24 +1,66 @@
+// const todoListContainer = document.querySelector('.todo__list');
+// const input = document.querySelector('.input');
+
+// function submit() {
+//     const todoList = document.createElement('div');
+//     const todocheckbox = document.createElement('input');
+//     const todospan = document.createElement('span');
+//     todocheckbox.type = "checkbox";
+
+//     todospan.textContent = input.value;
+//     todospan.style.textTransform = "capitalize";
+//     input.value = "";
+
+//     todoListContainer.appendChild(todoList);
+//     todoList.appendChild(todocheckbox);
+//     todoList.appendChild(todospan);
+    
+//     todocheckbox.addEventListener("click", function() {
+//         if (todocheckbox.checked) {
+//             todospan.style.textDecoration = "line-through";
+//         }else {
+//             todospan.style.textDecoration = "none";
+//         }
+//     })
+// }
+
+let tasks = [];
 const todoListContainer = document.querySelector('.todo__list');
-const input = document.querySelector('.input');
-const todoList = document.createElement('div');
-const todocheckbox = document.createElement('input');
-const todospan = document.createElement('span');
 
 function submit() {
-    todocheckbox.type = "checkbox";
-    todospan.textContent = input.value;
-    todospan.style.textTransform = "capitalize";
+    const input = document.querySelector('.input')
+    if (input.value === "") return
+    tasks.push({value:input.value, checked:false})
     input.value = "";
-
-    todoListContainer.appendChild(todoList);
-    todoList.appendChild(todocheckbox);
-    todoList.appendChild(todospan);
+    append();
 }
 
-todocheckbox.addEventListener("click", function() {
-    if (todocheckbox.checked) {
-        todospan.style.textDecoration = "line-through";
-    }else {
-        todospan.style.textDecoration = "none";
+function append() {
+    todoListContainer.innerHTML = "";
+    for(let i=0; i<tasks.length; i++) {
+        const todoList = document.createElement('div');
+        const todocheckbox = document.createElement('input');
+        const todospan = document.createElement('span');
+    
+        todoList.dataset.index = i;
+        todocheckbox.type = "checkbox";
+        todospan.textContent = tasks[i].value;
+        todospan.style.textTransform = "capitalize";
+        todocheckbox.checked = tasks[i].checked;
+    
+        todoListContainer.appendChild(todoList);
+        todoList.appendChild(todocheckbox);
+        todoList.appendChild(todospan);
+        
+        if(tasks[i].checked === true) {
+            todospan.style.textDecoration = "line-through";
+            todocheckbox.checked = true;
+        }
+
+        todoList.addEventListener("click", function done() {
+            tasks[i].checked = true;
+            append();
+        })
+
     }
-})
+}
